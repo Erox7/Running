ea46017e3bd74a6d7afb8cb0ae4195cc20b625fa
Erox7 <trugove@hotmail.com>
2018-04-21 13:50:24 +0200

    LACK OF: - CONTROL THE STATE OF THE APP.(When some1 closes it) - FINAL ACTIVITY WITH THE RESULTS, DO IT WITH THE MUSCLE, BECOUSE WE NEED THE LOG TO BE READ FROM THE CLOUD. - DISCOVER HOW TO CONTROL THE CLICK OF THE MAP BETTER. - THE PURE MUSCLE ITSELF, LIKE LOGIN OR THE POINT NUMBER 2. - REGISTER FUNCTIONALITY
    Password at login now shows "*" instead of the whole password.

--- app/src/main/java/com/example/erox/running/RegisterActivity.java
+++ app/src/main/java/com/example/erox/running/RegisterActivity.java
@@ -1,4 +1,33 @@
 package com.example.erox.running;
 
-class RegisterActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity{
+    CheckBox checkbox;
+    EditText password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+        checkbox = findViewById(R.id.checkbox);
+        password = findViewById(R.id.password);
+    }
+
+    public void backToLogin(View view) {
+
+    }
+
+    public void showPasswordDetection(View view) {
+        if(((CheckBox)view).isChecked()){
+            password.setTransformationMethod(null);
+        }else{
+            password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+        }
+    }
 }
