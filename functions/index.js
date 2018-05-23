7e287b8f7d79664b78b79101d0686927f222a566
Erox7 <trugove@hotmail.com>
2018-05-23 17:14:35 +0200

    LACK OF: - CONTROL THE STATE OF THE APP.(When some1 closes it) - LOG TO BE WRITE TO THE CLOUD. - DISCOVER HOW TO CONTROL THE CLICK OF THE MAP BETTER. - NOTIFICATIONS PUSH, DATABASE USE.
    Changed index.js, now on firebase console it returns 'ok' code.

--- functions/index.js
+++ functions/index.js
@@ -2,10 +2,28 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+exports.sendWelcomeNotification = functions.database.ref('/User/{Uid}')
+    .onWrite((change, context) => {
+
+    var uid = context.params.Uid;
+    var name = admin.database().ref(`/User/${uid}/name`).once('value');
+
+	var payload = {
+		notification: {
+			title: `New message from the backEnd`,
+			body: 'Welcome' + name.toString() ,
+			icon: '/images/firebase_ic.png'
+		}
+	};
+	var token = admin.database().ref(`/User/${uid}/token/`).once('value');
+	return admin.messaging().sendToDevice(token.toString(), payload)
+
+});
+/*
 exports.sendWelcomeNotification = functions.auth.user().onCreate(event => {
 	const user = event.data;
 	const uid = user.uid;
-	const name = admin.database().ref('/User/{uid}/').child('name');
+	const name = admin.database().ref('/User/{uid}/').child('name').once('value');
 	
 	var payload = {
 		notification: {
@@ -14,6 +32,7 @@ exports.sendWelcomeNotification = functions.auth.user().onCreate(event => {
 			icon: '/images/firebase_ic.png'
 		}
 	};
-	var token = admin.database().ref('/User/{uid}/').child('token');
+	var token = admin.database().ref('/User/{uid}/').child('token').once('value');
 	return admin.messaging().sendToDevice(token, payload)
 });
+*/
