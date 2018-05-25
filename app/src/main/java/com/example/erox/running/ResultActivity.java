d8b74e686db5c5dc84f216e30cea61e02eceba0b
jpn6 <jpn6@alumnes.udl.cat>
2018-05-25 17:29:46 +0200

    Jordi part (prepared)

--- app/src/main/java/com/example/erox/running/ResultActivity.java
+++ app/src/main/java/com/example/erox/running/ResultActivity.java
@@ -1,15 +1,25 @@
 package com.example.erox.running;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class ResultActivity extends AppCompatActivity {
     RunningLogs log;
     TextView results;
-
+    String finalUrl,userName;
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase mData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,11 +32,44 @@ public class ResultActivity extends AppCompatActivity {
         } else {
             results.setText(getString(R.string.noResults));
         }
+        mAuth = FirebaseAuth.getInstance();
+        mData = FirebaseDatabase.getInstance();
 
     }
 
     public void returnToMapFunction(View view) {
         Intent i = new Intent(this, MapsActivity.class);
         startActivity(i);
+    }
+
+    public void returnToServer(View view) {
+        prepareString();
+        goToUrl(finalUrl);
+    }
+
+    private void prepareString() {
+        String uid = mAuth.getCurrentUser().getUid();
+        DatabaseReference name = mData.getReference("/User/" + uid + "/name");
+        name.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                userName = dataSnapshot.getValue().toString();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        finalUrl = "https://peaceful-peak-99231.herokuapp.com/timePass?";
+        finalUrl += "time=" + log.getTimeInSeconds();
+        finalUrl += "&avgTime=" + log.getAvgWalkingTime().toString();
+        finalUrl += "&name=" + userName;
+    }
+
+    private void goToUrl(String s) {
+        Uri uriUrl = Uri.parse(s);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
     }
 }
